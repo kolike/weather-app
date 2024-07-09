56a7b4818b6f7c8542ccf8a1a2c5ace86d1ff198
Nikolay <nikolasfonpetroff@gmail.com>
2024-07-09 11:49:10 +0300

    change path img

--- src/WeatherInfo.vue
+++ src/WeatherInfo.vue
@@ -31,7 +31,7 @@ const showWind = computed(() => {
 })
 
 const weatherImgSrc = computed(() => {
-    return `src/img/${props.weather.toLowerCase()}.png`
+    return `/src/img/${props.weather.toLowerCase()}.png`
 })
 </script>
 <template>
